198fcfa1e676a814a1bcc19fb1ae5b1167b87077
Michael <M65C02A@gmail.com>
2020-04-27 23:46:04 -0500

    Updated the ReadInCSV.py to read and parse the CSV file provided by Altium Schematic.

--- ReadInCSV.py
+++ ReadInCSV.py
@@ -17,7 +17,37 @@ else:
 
     with open(filename+'.bom', "wt") as fout:
         for line in lines:
-            print(line)
+##            print(line)
             print(line, file=fout, end='')
+
+    with open(filename+'.lst', "wt") as fout:
+        srcLines = []
+        ln = int(0)
+        for line in lines:
+            ln += 1
+            if ln == 1:
+                firstLine = line
+            elif ln == 2:
+                secondLine = line
+            elif ln == 3 or ln == 5:
+                pass
+            elif ln == 4:
+                fourthLine = line
+                titles = fourthLine.split(",")
+                numTitles = len(titles)
+                for j in range(numTitles):
+                    if j != (numTitles - 1):
+                        titles[j] = titles[j].strip('"')
+                    else:
+                        titles[j] = titles[j].lstrip('"').rstrip('"\n')
+                print(titles)
+                print(titles, file=fout)
+            else:
+                fields = line.split('","')
+                fields[0] = fields[0].lstrip('"')
+                fields[4] = fields[4].rstrip('"\n')
+                print(fields)
+                print(fields, file=fout)
+                srcLines.append(fields)
             
 
